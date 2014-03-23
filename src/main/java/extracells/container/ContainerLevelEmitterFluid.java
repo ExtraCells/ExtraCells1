9cec01f29e951ca1a03d226c2d3b4cf77814ff48
M3gaFr3ak <leon.loeser@yahoo.de>
2014-03-23 10:10:48 +0100

    forgot import -.-#

--- src/main/java/extracells/container/ContainerLevelEmitterFluid.java
+++ src/main/java/extracells/container/ContainerLevelEmitterFluid.java
@@ -4,9 +4,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.fluids.FluidContainerRegistry;
-import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fluids.ItemFluidContainer;
+import net.minecraftforge.fluids.*;
 import extracells.ItemEnum;
 import extracells.container.slot.SlotFake;
 
